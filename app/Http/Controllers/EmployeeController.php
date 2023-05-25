351b745f9130a35ab8e4e405cc5b6091f1b209aa
esahahahihi <121810193+esahahahihi@users.noreply.github.com>
2023-05-25 21:58:08 +0700

    Add files via upload

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Employee;
+use Illuminate\Support\Facades\DB;
+
+class EmployeeController extends Controller
+{
+    public function index()
+    {
+
+        $pageTitle = 'Employee List';
+
+        // Query Builder
+        $employees = DB::table('employees')
+            ->leftJoin('positions', 'employees.position_id', '=', 'positions.id')
+            ->select('employees.*', 'employees.id as employee_id', 'positions.name as position_name')
+            ->get();
+
+        return view('employee.index', [
+            'pageTitle' => $pageTitle,
+            'employees' => $employees
+        ]);
+
+    }
+
+    public function create()
+    {
+        $pageTitle = 'Create Employee';
+
+        // Query Builder
+        $positions = DB::table('positions')->get();
+
+        return view('employee.create', compact('pageTitle', 'positions'));
+    }
+
+    public function store(Request $request)
+    {
+        $messages = [
+            'required' => ':attribute harus diisi.',
+            'email' => 'Isi :attribute dengan format yang benar.',
+            'numeric' => 'Isi :attribute dengan angka.'
+        ];
+        $validator = Validator::make($request->all(), [
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email',
+            'age' => 'required|numeric',
+        ], $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+
+        DB::table('employees')->insert([
+            'firstname' => $request->firstName,
+            'lastname' => $request->lastName,
+            'email' => $request->email,
+            'age' => $request->age,
+            'position_id' => $request->position,
+        ]);
+
+        return redirect()->route('employees.index');
+    }
+
+    public function show(string $id)
+    {
+        $pageTitle = 'Employee Detail';
+
+        // Query Builder
+        $employee = DB::table('employees')
+            ->leftJoin('positions', 'employees.position_id', '=', 'positions.id')
+            ->select('employees.*', 'employees.id as employee_id', 'positions.name as position_name')
+            ->where('employees.id', '=', $id)
+            ->first();
+
+        return view('employee.show', compact('pageTitle', 'employee'));
+
+
+    }
+    public function edit(string $id)
+    {
+        $pageTitle = 'Edit Employee';
+
+        // Query Builder
+        $employee = DB::table('employees')
+            ->leftJoin('positions', 'employees.position_id', '=', 'positions.id')
+            ->select('employees.*', 'employees.id as employee_id', 'positions.name as position_name')
+            ->where('employees.id', '=', $id)
+            ->first();
+
+        $positions = DB::table('positions')->get();
+
+        return view('employee.edit', compact('pageTitle', 'employee', 'positions'));
+
+
+    }
+
+    public function update(Request $request, $id)
+    {
+        $messages = [
+            'required' => ':attribute harus diisi.',
+            'email' => 'Isi :attribute dengan format yang benar.',
+            'numeric' => 'Isi :attribute dengan angka.'
+        ];
+
+        $validator = Validator::make($request->all(), [
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email',
+            'age' => 'required|numeric',
+        ], $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        DB::table('employees')
+            ->where('id', $id)
+            ->update([
+                'firstname' => $request->firstName,
+                'lastname' => $request->lastName,
+                'email' => $request->email,
+                'age' => $request->age,
+                'position_id' => $request->position,
+            ]);
+
+        return redirect()->route('employees.index');
+    }
+
+
+    public function destroy(string $id)
+    {
+        // QUERY BUILDER
+        DB::table('employees')
+        ->where('id', $id)
+        ->delete();
+
+    return redirect()->route('employees.index');
+
+    }
+}
